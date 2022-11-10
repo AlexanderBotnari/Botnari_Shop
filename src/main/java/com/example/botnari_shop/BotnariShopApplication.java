aeea6e627140e65d5358fcba3bcb1469efad8104
Alexandru <sandubotnari99@gmail.com>
2022-11-10 16:53:55 +0200

    init WEB UI

--- src/main/java/com/example/botnari_shop/BotnariShopApplication.java
+++ src/main/java/com/example/botnari_shop/BotnariShopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BotnariShopApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BotnariShopApplication.class, args);
     }
 
