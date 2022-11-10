0801bf732dbe4652a041dd7605da173694835909
Alexandru <sandubotnari99@gmail.com>
2022-11-10 13:37:57 +0200

    init

---
+++ src/main/java/com/example/botnari_shop/BotnariShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.botnari_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotnariShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotnariShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/botnari_shop/BotnariShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.botnari_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotnariShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
